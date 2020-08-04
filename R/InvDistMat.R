8428421fda00854a9f3263bbd482db4587bd6921
Rozeta Simonovska <61285321+RozetaSimonovska@users.noreply.github.com>
2020-08-04 11:07:07 +0200

    Update InvDistMat.R

--- R/InvDistMat.R
+++ R/InvDistMat.R
@@ -37,17 +37,18 @@ InvDistMat<-function(distMat,distCutOff = NULL,powr = 1,metres = TRUE){
     if(!metres){
       distMat<-distMat/1000
       if(!is.null(distCutOff)){distCutOff<-distCutOff/1000}
+      distMat[which(distMat<1 & distMat>0)]<-1.001
       }
     for(i in 1:(n-1)){
       if(!is.null(distCutOff)){
-        for(j in which(distMat[i,]<distCutOff)){
+        for(j in which(distMat[i,]<distCutOff & distMat[i,]!=0)){
           if(j>i){
             W[i,j]<-1/(distMat[i,j])^powr
             W[j,i]<-1/(distMat[i,j])^powr
           }
         }
       } else{
-        for(j in (i+1):n){
+        for(j in which(distMat[i,]!=0)){
           W[i,j]<-1/(distMat[i,j])^powr
           W[j,i]<-1/(distMat[i,j])^powr
         }
